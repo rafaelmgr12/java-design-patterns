42e43e259e55dd428c446e36f1a0cdde79238a0a
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-20 22:32:13 -0300

    Upadate Test

--- src/test/java/TestDiscount.java
+++ src/test/java/TestDiscount.java
@@ -14,4 +14,36 @@ public class TestDiscount {
         BigDecimal result = computeDiscount.compute(budget);
         assertEquals(new BigDecimal("0"), result);
     }
+
+    @Test
+    public void testDiscountWithMoreThanFiveItems() {
+        Budget budget = new Budget(new BigDecimal("100"), 6);
+        ComputeDiscount computeDiscount = new ComputeDiscount();
+        BigDecimal result = computeDiscount.compute(budget);
+        assertEquals(new BigDecimal("10.0"), result);
+    }
+
+    @Test
+    public void testDiscountWithMoreThanFiveHundredReais() {
+        Budget budget = new Budget(new BigDecimal("600"), 1);
+        ComputeDiscount computeDiscount = new ComputeDiscount();
+        BigDecimal result = computeDiscount.compute(budget);
+        assertEquals(new BigDecimal("30.00"), result);
+    }
+
+    @Test
+    public void testDiscountWithMoreThanFiveItemsAndMoreThanFiveHundredReais() {
+        Budget budget = new Budget(new BigDecimal("600"), 6);
+        ComputeDiscount computeDiscount = new ComputeDiscount();
+        BigDecimal result = computeDiscount.compute(budget);
+        assertEquals(new BigDecimal("30.00"), result);
+    }
+
+    @Test
+    public void testDiscountWithMoreThanFiveItemsAndMoreThanFiveHundredReaisAndMoreThanFiveItemsAndMoreThanFiveHundredReais() {
+        Budget budget = new Budget(new BigDecimal("600"), 6);
+        ComputeDiscount computeDiscount = new ComputeDiscount();
+        BigDecimal result = computeDiscount.compute(budget);
+        assertEquals(new BigDecimal("30.00"), result);
+    }
 }
